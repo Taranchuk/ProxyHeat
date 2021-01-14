33d41552f5bc969ffb5cb5b62a23c88a20eb1ae5
Taranchuk <nfhfyxer@gmail.com>
2021-01-14 15:52:18 +0200

    Initial commit

---
+++ 1.2/Source/ProxyHeat/Comps/CompTemperatureSource.cs
@@ -0,0 +1,187 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace ProxyHeat
+{
+	public class CompProperties_TemperatureSource : CompProperties
+	{
+		public int radius;
+		public float tempOutcome;
+		public bool dependsOnPower;
+		public bool dependsOnFuel;
+		public CompProperties_TemperatureSource()
+		{
+			compClass = typeof(CompTemperatureSource);
+		}
+	}
+
+	public class CompTemperatureSource : ThingComp
+    {
+		public CompProperties_TemperatureSource Props => (CompProperties_TemperatureSource)props;
+		private bool active;
+		private Map map;
+		public bool Active => active;// && position.UsesOutdoorTemperature(map);
+		private CompPowerTrader powerComp;
+		private CompRefuelable fuelComp;
+		public IntVec3 position;
+		private HashSet<IntVec3> affectedCells = new HashSet<IntVec3>();
+		public HashSet<IntVec3> AffectedCells => affectedCells;
+		private List<IntVec3> affectedCellsList = new List<IntVec3>();
+		private bool dirty;
+		public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+			if (Props.dependsOnPower)
+            {
+				powerComp = this.parent.GetComp<CompPowerTrader>();
+            }
+			if (Props.dependsOnFuel)
+            {
+				fuelComp = this.parent.GetComp<CompRefuelable>();
+			}
+			this.position = this.parent.Position;
+			this.map = this.parent.Map;
+			this.dirty = true;
+			if (HarmonyInit.compTemperatureSources.ContainsKey(map))
+            {
+				HarmonyInit.compTemperatureSources[map].Add(this);
+			}
+			else
+            {
+				HarmonyInit.compTemperatureSources[map] = new List<CompTemperatureSource> { this };
+			}
+		}
+
+        public override void PostPostMake()
+        {
+            base.PostPostMake();
+        }
+
+		public void MarkDirty()
+        {
+			this.dirty = true;
+        }
+        public void RecalculateAffectedCells()
+        {
+			Log.Message(this + " - RecalculateAffectedCells");
+			affectedCells.Clear();
+			affectedCellsList.Clear();
+			Func<IntVec3, bool> validator = delegate (IntVec3 cell)
+			{
+				if (!cell.Walkable(map))
+                {
+					return false;
+                }
+				var edifice = cell.GetEdifice(map);
+				var result = edifice == null || edifice.def.passability != Traversability.Impassable;
+				return result;
+			};
+			foreach (var intVec in GenRadial.RadialCellsAround(position, Props.radius, true))
+			{
+				if (GenSight.LineOfSight(position, intVec, map, validator: validator))
+                {
+					var edifice = intVec.GetEdifice(map);
+					if (edifice == null || edifice.def.passability != Traversability.Impassable)
+                    {
+						if (!this.parent.OccupiedRect().Contains(intVec))
+                        {
+							affectedCells.Add(intVec);
+                        }
+					}
+				}
+			}
+			affectedCellsList.AddRange(affectedCells.ToList());
+			this.dirty = false;
+		}
+        public override void PostDrawExtraSelectionOverlays()
+        {
+            base.PostDrawExtraSelectionOverlays();
+			Log.Message(affectedCellsList.Count.ToString());
+			if (Props.tempOutcome >= 0)
+            {
+				GenDraw.DrawFieldEdges(affectedCellsList, GenTemperature.ColorRoomHot);
+            }
+			else
+            {
+				GenDraw.DrawFieldEdges(affectedCellsList, GenTemperature.ColorRoomCold);
+			}
+		}
+		public override void PostDeSpawn(Map map)
+        {
+            base.PostDeSpawn(map);
+			if (HarmonyInit.compTemperatureSources.ContainsKey(map))
+			{
+				HarmonyInit.compTemperatureSources[map].Remove(this);
+			}
+		}
+
+		public override void CompTick()
+        {
+            base.CompTick();
+			if (!Props.dependsOnFuel && !Props.dependsOnPower)
+            {
+				if (!this.active)
+                {
+					this.active = true;
+					this.dirty = true;
+				}
+            }
+			if (powerComp != null)
+            {
+				if (!powerComp.PowerOn) this.active = false;
+				else if (!this.active)
+				{
+					this.active = true;
+					this.dirty = true;
+				}
+			}
+			if (fuelComp != null)
+            {
+				if (!fuelComp.HasFuel) this.active = false;
+				else if (!this.active)
+				{
+					this.active = true;
+					this.dirty = true;
+				}
+            }
+			if (!this.active && this.affectedCells.Any())
+            {
+				this.affectedCells.Clear();
+				this.affectedCellsList.Clear();
+            }
+			else if (this.dirty)
+			{
+				RecalculateAffectedCells();
+			}
+		}
+
+		public bool IsNearby(IntVec3 nearByCell)
+        {
+			if (affectedCells.Contains(nearByCell))
+            {
+				return true;
+            }
+			return false;
+        }
+
+		public bool InRange(IntVec3 nearByCell)
+		{
+			if (this.position.DistanceTo(nearByCell) <= Props.radius)
+			{
+				return true;
+			}
+			return false;
+		}
+		public override void PostExposeData()
+        {
+            base.PostExposeData();
+			Scribe_Values.Look(ref active, "active");
+        }
+    }
+}
---
+++ 1.2/Source/ProxyHeat/HarmonyPatches/HarmonyPatches.cs
@@ -0,0 +1,150 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+
+namespace ProxyHeat
+{
+	[StaticConstructorOnStartup]
+	internal static class HarmonyInit
+	{
+		public static Dictionary<Map, List<CompTemperatureSource>> compTemperatureSources = new Dictionary<Map, List<CompTemperatureSource>>();
+		static HarmonyInit()
+		{
+			Harmony harmony = new Harmony("LongerCFloor.ProxyHeat");
+			harmony.PatchAll();
+		}
+
+		[HarmonyPatch(typeof(Building), nameof(Building.SpawnSetup))]
+		public static class Patch_SpawnSetup
+		{
+			private static void Postfix(Building __instance)
+			{
+				var thingMap = __instance.Map;
+				if (thingMap != null && compTemperatureSources.TryGetValue(thingMap, out List<CompTemperatureSource> tempSources))
+				{
+					var position = __instance.Position;
+					for (int i = 0; i < tempSources.Count; i++)
+					{
+						var compTempSource = tempSources[i];
+						if (compTempSource.Active && compTempSource.IsNearby(position))
+						{
+							compTempSource.MarkDirty();
+						}
+					}
+				}
+			}
+		}
+
+		[HarmonyPatch(typeof(Building), nameof(Building.DeSpawn))]
+		public static class Patch_DeSpawn
+		{
+			private static void Prefix(Building __instance)
+			{
+				var thingMap = __instance.Map;
+				if (thingMap != null && compTemperatureSources.TryGetValue(thingMap, out List<CompTemperatureSource> tempSources))
+				{
+					var position = __instance.Position;
+					for (int i = 0; i < tempSources.Count; i++)
+					{
+						var compTempSource = tempSources[i];
+						if (compTempSource.Active && compTempSource.InRange(position))
+						{
+							compTempSource.MarkDirty();
+						}
+					}
+				}
+			}
+		}
+
+
+		[HarmonyPatch(typeof(Thing), nameof(Thing.AmbientTemperature), MethodType.Getter)]
+		public static class Patch_AmbientTemperature
+		{
+			private static void Postfix(Thing __instance, ref float __result)
+			{
+				var thingMap = __instance.Map;
+				if (thingMap != null && compTemperatureSources.TryGetValue(thingMap, out List<CompTemperatureSource> tempSources) && __instance is Pawn)
+                {
+					var tempSourceCandidates = new List<CompTemperatureSource>();
+					var position = __instance.Position;
+					for (int i = 0; i < tempSources.Count; i++)
+					{
+						var compTempSource = tempSources[i];
+						if (compTempSource.Active && compTempSource.IsNearby(position))
+                        {
+							tempSourceCandidates.Add(compTempSource);
+                        }
+					}
+					if (tempSourceCandidates.Any())
+                    {
+						var tempSource = tempSourceCandidates.OrderBy(x => x.position.DistanceTo(position)).First();
+						__result = tempSource.Props.tempOutcome;
+					}
+				}
+			}
+		}
+
+
+		[HarmonyPatch(typeof(JobGiver_SeekSafeTemperature), "TryGiveJob")]
+		public static class Patch_TryGiveJob
+		{
+			private static bool Prefix(Pawn pawn, ref Job __result)
+            {
+				if (!pawn.health.hediffSet.HasTemperatureInjury(TemperatureInjuryStage.Serious))
+				{
+					return false;
+				}
+				FloatRange tempRange = pawn.ComfortableTemperatureRange();
+				if (!tempRange.Includes(pawn.AmbientTemperature))
+				{
+					var job = SeekSafeTemperature(pawn, tempRange);
+					if (job != null)
+                    {
+						__result = job;
+						return false;
+                    }
+				}
+				return true;
+			}
+
+			private static Job SeekSafeTemperature(Pawn pawn, FloatRange tempRange)
+			{
+				Log.Message("SeekSafeTemperature: " + pawn);
+				var map = pawn.Map;
+				if (pawn.Position.UsesOutdoorTemperature(map) && HarmonyInit.compTemperatureSources.TryGetValue(map, out List<CompTemperatureSource> temperatureSources))
+				{
+					var candidates = temperatureSources.Where(x => tempRange.Includes(x.Props.tempOutcome)).OrderBy(x => pawn.Position.DistanceTo(x.position)).ToList();
+					while (candidates.Any())
+					{
+						var candidate = candidates.First();
+						var cells = candidate.AffectedCells.OrderBy(x => candidate.position.DistanceTo(x)).ToList();
+						while (cells.Any())
+                        {
+							var list = cells.Take(10).InRandomOrder();
+							cells = cells.Skip(10).ToList();
+							foreach (var affectedCell in list)
+							{
+								if (pawn.CanReserveAndReach(affectedCell, PathEndMode.OnCell, Danger.Deadly))
+								{
+									Log.Message("Return job: " + pawn + " - " + affectedCell);
+									return JobMaker.MakeJob(JobDefOf.GotoSafeTemperature, affectedCell);
+								}
+							}
+						}
+						candidates.Remove(candidate);
+					}
+				}
+				Log.Message("Return null - SeekSafeTemperature: " + pawn);
+				return null;
+			}
+		}
+	}
+}
